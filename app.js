9c4909b776284f5e48f546d301873193c6674c92
Naman <naman.aggarwal@grabtaxi.com>
2019-03-21 14:25:12 +0800

    Added id_token_hint

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function initiateOAuthFlow(){
   //1. Get the id_token from the URL parameter
+  const id_token = "";
 
   //2. Set the config parameters
   const openIdUrl = GrabID.getGrabUrls().PRODUCTION;
@@ -7,7 +8,8 @@ function initiateOAuthFlow(){
     clientId: 'put here your clientID', //Needs to be your clientID
     redirectUri: 'http://dev.naman.com:8000/callback.html', // Needs to be your redirect URL
     scope: ['openid', 'profile.read'].join(' '), // Needs to be the scope you need
-    acrValues: ['consent_ctx:country=sg'].join(' ')
+    acrValues: ['consent_ctx:country=sg'].join(' '),
+    id_token_hint: id_token
   };
 
   //3. Get the grabIDClient
